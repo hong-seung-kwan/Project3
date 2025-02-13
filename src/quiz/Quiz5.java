9644e3a42681cefdf1660af74ffa295a8fcb9b0f
hongseungkwan <hh8164@naver.com>
2025-02-13 17:04:41 +0900

    수정

---
+++ src/quiz/Quiz5.java
@@ -0,0 +1,27 @@
+package quiz;
+
+public class Quiz5 {
+
+	public static void main(String[] args) {
+		// 본인의 나이가 40세 이하인지?
+				// 본인의 나이가 40이 맞는지?
+				// 본인의 나이가 40이 아닌지?
+				
+				int age = 28;
+				
+				System.out.println(age <= 40); //
+				System.out.println(age == 40);
+				System.out.println(age != 40);
+				
+//				boolean result1 = (age <= 40);
+//				System.out.println("본인의 나이가 40세 이하인지?" + result1);
+//				
+//				boolean result2 = (age == 40);
+//				System.out.println("본인의 나이가 40이 맞는지?" + result2);
+//				
+//				boolean result3 = (age != 40);
+//				System.out.println("본인의 나이가 40이 아닌지?" + result3);
+
+	}
+
+}
